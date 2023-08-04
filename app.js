381d648aba456d0406613ffc389774e4e134788c
Tamnguyen753 <nguyendactam0299@gmail.com>
2023-08-04 20:51:55 +0700

    message

---
+++ app.js
@@ -0,0 +1,21 @@
+const input = document.getElementById("input");
+const button = document.getElementById("button-id");
+const todoLists = document.getElementById("list-todo");
+const clearBtn = document.getElementById("clear-btn");
+
+button.addEventListener("click", () => {
+  const value = input.value;
+  if (value === "") {
+    alert("Nhap day du vao");
+  } else {
+    const li = document.createElement("li");
+    li.innerText = value;
+    todoLists.appendChild(li);
+    input.value = "";
+    alert("Success");
+  }
+});
+
+clearBtn.addEventListener("click", () => {
+  todoLists.innerText = "";
+});
---
+++ readme.md
@@ -0,0 +1,20 @@
+//function
+1 : Là hàm;
+2 : tiết kiệm code
+scope : global và local
+3 : tạo hàm
+3.1 : function keyword : hoisting, có this
+3.2 : arrow function : k có, k có this
+this
+
+//DOM
+1 là gỉ : Gia phả
+node
+cách truy vấn đến node
+id, class, tagName
+id : getElementByid -> 1 ptu
+class, tagName -> HTML Collections
+
+//thêm phần tử vào dom
+//b1 : tạo ptu
+//b2 : thêm vào dom
